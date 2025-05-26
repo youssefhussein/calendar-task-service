710f56cc3d12d42b631217e251e5b27d75039c8f
youssef hussein <20634930+youssefhussein@users.noreply.github.com>
2025-05-26 19:17:49 +0300

    init again

---
+++ src/main/java/com/calendar/tasks/calendartaskservice/CalendarTaskServiceApplication.java
@@ -0,0 +1,13 @@
+package com.calendar.tasks.calendartaskservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarTaskServiceApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarTaskServiceApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/calendar/tasks/calendartaskservice/CalendarTaskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calendar.tasks.calendartaskservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarTaskServiceApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
